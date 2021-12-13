7a56f6e4088c5933d940fcfc476674270133d71c
DivyanshiChouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2021-12-13 23:38:01 +0530

    Create non constructible change.py

---
+++ non constructible change.py
@@ -0,0 +1,11 @@
+# O(n) Time complexity | O(1) Space complexity
+def nonConstructibleChange(coins):
+    coins.sort()
+    currentSum = 0 
+    for coin in coins:
+        if coin > currentSum +1:
+            return currentSum +1
+        currentSum += coin
+    return currentSum +1
+
+print(nonConstructibleChange([1,1,3,4]))
