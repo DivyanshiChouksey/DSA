f277aab29148fa0f3e8d98591311395da876df7d
DivyanshiChouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2022-02-04 22:37:29 +0530

    Update valid palindrome.py

--- valid palindrome.py
+++ valid palindrome.py
@@ -1,8 +1,40 @@
-# palindrome or not
+# Valid Palindrome
 s = "A man, a plan, a canal: Panama"
+
+# Naive Solution
 st = ""
-for ch in s :
+for ch in s:
     if ch.isalnum():
         st+=(ch.lower())
+if st[::-1] == st:
+    print (True)
+else:
+    print (False)
+    
+# For Loop
+st = ""
+for ch in s:
+    if ch.isalnum():
+        st+=(ch.lower())
+start = 0
+end = -1
+for i in range(len(st)//2):
+    if st[start]!=st[end]:
+        print (False)
+    start+=1
+    end-=1
+print (True)
 
-print(True if st[::-1]== st else False)
+# While Loop
+st = ""
+for ch in s:
+    if ch.isalnum():
+        st+=(ch.lower())
+start = 0
+end = len(st)-1
+while (start<end):
+    if st[start]!=st[end]:
+        print(False)
+    start+=1
+    end-=1
+print (True)
